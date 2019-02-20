c1f1aa0eae2e79130ff73893f84b9cb2fb04e979
Amelia Lunggita Sapna <46413690+Lunggita29@users.noreply.github.com>
2019-02-20 14:54:49 +0700

    Add files via upload

---
+++ latihan4.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Ameliaaaa";
+function tampil_nama(){
+    global $nama;
+    echo "Nama Saya : ".$nama;
+}
+tampil_nama();
+?>
---
+++ variabel_hasil.php
@@ -0,0 +1,10 @@
+<?php
+echo $_POST['nama'];
+echo "<br>";
+echo $_POST['email'];
+?>
+
+<!--
+    method post untuk menampilkan
+    method get untuk mengambil nilai
+-->
